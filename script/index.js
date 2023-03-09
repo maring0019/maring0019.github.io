45b398f36162f6e772ffbf51f2a004dce8728b59
Mariela Benicio <maring0019@gmail.com>
2023-03-08 21:31:44 -0300

    Primer commit.

---
+++ script/index.js
@@ -0,0 +1,12 @@
+
+
+
+function mostrar(detalle) {
+    var div = document.getElementById(detalle);
+    div.style.display = '';
+    }
+    
+    function cerrar(detalle) {
+    var div = document.getElementById(detalle);
+    div.style.display = 'none';
+    }
